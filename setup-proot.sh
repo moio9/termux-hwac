42e9f31e959c43ee3418c178321ab6cddfa00556
moio9 <111084587+moio9@users.noreply.github.com>
2024-04-03 23:33:32 +0300

    Update setup-proot.sh pulse

--- setup-proot.sh
+++ setup-proot.sh
@@ -12,9 +12,8 @@ function alias_proot {
   path=/data/data/com.termux/files/usr/bin
   echo 'Proot Desktop alias is hwac'
   echo '#!/usr/bin/bash'>"$path/hwac"
-  echo "path="$path>>"$path/hwac"
+  echo $path"/termux11 k">>"$path/hwac"
   echo "pulseaudio --start --exit-idle-time=-1 &">>"$path/hwac"
-  echo "pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1 &">>"$path/hwac"
   echo "am start -n com.termux.x11/com.termux.x11.MainActivity">>"$path/hwac"
   echo 'proot-distro login debian-hwac --shared-tmp -- bash termux11 $1'>>"$path/hwac"
 
