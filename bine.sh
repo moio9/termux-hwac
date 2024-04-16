d9bbf240a060b9ca40def7f5572234efb274e9f7
moio9 <111084587+moio9@users.noreply.github.com>
2024-04-16 19:41:47 +0300

    vulkan box

---
+++ bine.sh
@@ -0,0 +1,25 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+export WINE_PATH=$PREFIX/glibc/wine
+export WINEPREFIX=$PREFIX/glibc/.wine
+
+export WINEESYNC=1
+export WINEESYNC_TERMUX=1
+
+export WINEDLLOVERRIDES="d3d11,d3d10,dxgi,d3d9=n,b"
+
+export VK_ICD_FILENAMES=$PREFIX/glibc/share/vulkan/icd.d/freedreno_icd.aarch64.json
+export DXVK_ASYNC=1
+
+export BOX64_PATH=$PREFIX/glibc/bin
+export BOX64_MMAP32=1
+
+export GLIBC_BIN=$PREFIX/glibc/bin
+LD_PRELOAD_SAVED=$LD_PRELOAD
+unset LD_PRELOAD
+
+
+$GLIBC_BIN/box64 $WINE_PATH/bin/wine "$@"
+
+
+export LD_PRELOAD=$LD_PRELOAD_SAVED
---
+++ dxvk_in.sh
@@ -0,0 +1,23 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+version=2.3.1-1
+
+if [ -d 'dxvk-gplasync-v$version' ]; then
+	cd dxvk-gplasync-v$version
+else
+	mkdir $PREFIX/glibc/dxvk
+	cd $PREFIX/glibc/dxvk
+	wget https://gitlab.com/Ph42oN/dxvk-gplasync/-/raw/main/releases/dxvk-gplasync-v$version.tar.gz
+	tar -xvf dxvk-gplasync-v$version.tar.gz
+	cd dxvk-gplasync-v$version
+fi
+
+if [ -v $WINEPREFIX ]; then
+	export WINEPREFIX=$PREFIX/glibc/.wine
+fi
+
+cp x64/*.dll $WINEPREFIX/drive_c/windows/system32
+cp x32/*.dll $WINEPREFIX/drive_c/windows/syswow64
+ 
+cd ..
+gio trash dxvk-gplasync-v$version.tar.gz
