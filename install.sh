f1c23ccac06dfbe636edf053a6bb5b0658fd18af
moio9 <111084587+moio9@users.noreply.github.com>
2024-04-16 20:37:18 +0300

    Update install.sh ln

--- install.sh
+++ install.sh
@@ -112,8 +112,8 @@ proot-distro login $name --shared-tmp -- $proot_arg
 fi
 
 termux-libs
-cp bine.sh $PREFIX/glibc/bin/bine
-ln -s $PREFIX/glibc/bin/bin/bine $PREFIX/bin
+ln -s bine.sh $PREFIX/glibc/bin/bine
+ln -s bine.sh $PREFIX/bin
 bine boot
 dxvk_in
 launcher
