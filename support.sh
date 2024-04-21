f6eb2280d8bb0c4352ebcc26000ef700cbee0dfa
moio9 <111084587+moio9@users.noreply.github.com>
2024-04-21 19:09:56 +0300

    Create support.sh Pleas ;(

---
+++ support.sh
@@ -0,0 +1,29 @@
+echo "
+######################################
+#Done!#
+######################################
+┌────────────────────────────────────┐
+│    Please help keep this project   │
+│             alive:                 │
+│                                    │
+├────────────────────────────────────┤
+│    Monero:   46Mk8t9uLY7jnBXnyHM   │
+│              yVARvwk1Y7jcGEQwKLN8  │
+│              GtGGBioncjKLgkEa33jE  │
+│              N2ibgkQjoFZWVwXXwsM3  │
+│              vzAFz4RzV7psLow6      │
+│                                    │
+├────────────────────────────────────┤
+│    Bitcoin:  bc1qgxp74eza7jaf4fdw  │
+│              5cl3sanqvnh0cjmz0w9scz│
+│                                    │
+├────────────────────────────────────┤
+│    Ethereum: 0xa024a505Ec24c7eA16  │
+│               3985eC89D56e614B9AdAa│
+│               e                    │
+│                                    │
+├────────────────────────────────────┤
+│    PayPal:   paypal.me/moioyoyo    │
+└────────────────────────────────────┘
+  moioyoyo76@gmail.com
+"
