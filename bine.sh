f953e35e8a50cfb016adb23bb574d6c33823c60a
moio9 <111084587+moio9@users.noreply.github.com>
2024-04-28 04:56:19 +0300

    Update bine.sh SOUND
    Sound + some quality of life

--- bine.sh
+++ bine.sh
@@ -1,15 +1,17 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 export WINE_PATH=$PREFIX/glibc/wine
-export WINEPREFIX=$PREFIX/glibc/.wine
+if [ -z "$WINEPREFIX" ]; then
+    export WINEPREFIX=$PREFIX/glibc/.wine
 
 export WINEESYNC=1
 export WINEESYNC_TERMUX=1
 
-export WINEDLLOVERRIDES="d3d11,d3d10,dxgi,d3d9=n,b"
+export WINEDLLOVERRIDES="${WINEDLLOVERRIDES},d3d11,d3d10,dxgi,d3d9=n,b"
 
 export VK_ICD_FILENAMES=$PREFIX/glibc/share/vulkan/icd.d/freedreno_icd.aarch64.json
 export DXVK_ASYNC=1
+export MESA_VK_WSI_PRESENT_MODE=mailbox
 
 export BOX64_PATH=$PREFIX/glibc/bin
 export BOX64_MMAP32=1
@@ -17,6 +19,10 @@ export BOX64_MMAP32=1
 export GLIBC_BIN=$PREFIX/glibc/bin
 LD_PRELOAD_SAVED=$LD_PRELOAD
 unset LD_PRELOAD
+
+unset PULSE_SERVER
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+export PULSE_SERVER=127.0.0.1
 
 
 $GLIBC_BIN/box64 $WINE_PATH/bin/wine "$@"
