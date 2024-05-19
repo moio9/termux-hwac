cc506520fc9c713b8e800e0a9e62b4a03f30b063
moio9 <111084587+moio9@users.noreply.github.com>
2024-05-19 03:32:59 +0300

    Update install.sh virgl

--- install.sh
+++ install.sh
@@ -143,7 +143,8 @@ pkg install -y \
 pkg install -y pulseaudio-glibc libx*-*glibc*
 pkg install -y libgmp-glibc
 pkg install -y fex
-pkg install -y mesa-zink-dev virgl_test_server* freetype gnutls \
+pkg install -y mesa-zink-dev virglrenderer-android virglrenderer-mesa-zink \
+    virgl_test_server* freetype gnutls \
     libandroid-shmem-static libx11 xorgproto libdrm libpixman libxfixes \
     libjpeg-turbo mesa-demos osmesa pulseaudio termux-x11-nightly vulkan-tools \
     xtrans libxxf86vm xorg-xrandr xorg-font-util xorg-util-macros libxfont2 \
