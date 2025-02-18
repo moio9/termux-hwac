b37108c79ad419cc122615ea7a261e187d7f59c2
moio9 <111084587+moio9@users.noreply.github.com>
2025-02-18 18:32:48 +0200

    Update wine_tweaks.sh

--- wine_tweaks.sh
+++ wine_tweaks.sh
@@ -5,12 +5,14 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 PREFIX_PATH1=$HOME/.wine
 PREFIX_PATH2=/data/data/com.termux/files/usr/glibc/wine
 
-if [ $1 == "hangover" ]; then
+if [ "$1" == "hangover" ]; then
     hangover-wine regedit $SCRIPT_DIR/tools/pre_fix.reg
     echo "Prefix $PREFIX_PATH1 creat și configurat!"
+    exit
 fi
 
-if [ $1 != "hangover ]; then
+if [ "$1" != "hangover" ]; then
     bine regedit $SCRIPT_DIR/tools/pre_fix.reg
     echo "Prefix $PREFIX_PATH2 creat și configurat!"
+    exit
 fi
