e04619941a2472f64d47ccead2fed8e6339403cb
moio9 <111084587+moio9@users.noreply.github.com>
2025-02-18 18:35:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -273,7 +273,8 @@ chmod +x wine_in.sh
 cp bine.sh $PREFIX/glibc/bin/bine
 ln -s $PREFIX/glibc/bin/bine $PREFIX/bin
 bine boot
-./wine_tweaks.sh
+./wine_tweaks.sh hang
+./wine_tweaks.sh bine
 ./dxvk_in.sh
 launcher
 pkg upgrade
